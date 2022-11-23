e0a7712725c3399bdeb50beefb09d9a50aafbaa5
SAEKay <114543365+SAEKay@users.noreply.github.com>
2022-11-23 12:06:56 +1100

    Initial commit

---
+++ Assets/Scripts/Webs.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KayBarhoum 
+{
+    public class Webs : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 22T3_GAD170.3_Barhoum_Kay
+ Project 3 working on a 3D game
